c9ddf8d762cbb9d8f3a5f1dd6b9837a888314a67
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-01 18:23:22 +0700

    Different println() & print()

--- Programming-With-IntelliJ-IDEA/kotlin-starter/src/main/kotlin/App.kt
+++ Programming-With-IntelliJ-IDEA/kotlin-starter/src/main/kotlin/App.kt
@@ -1,3 +1,9 @@
 fun main() {
     println("Hello Kotlin")
+
+    // println or print
+    val name = "John"
+    print("Hello my name is ")
+    println(name)
+    print(if (true) "Always true" else "Always false")
 }
