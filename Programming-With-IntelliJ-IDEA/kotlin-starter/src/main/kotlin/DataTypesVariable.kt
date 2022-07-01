5549b422ac2897a163f90d0860bb81c6e62670f6
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-01 18:26:43 +0700

    Data Types & Variable

---
+++ Programming-With-IntelliJ-IDEA/kotlin-starter/src/main/kotlin/DataTypesVariable.kt
@@ -0,0 +1,16 @@
+fun main() {
+    // var = public & val = private
+    // var identifier: Type = initialization
+    var company: String = "Dicoding"
+    println(company)
+    company = "Dicoding Academy"
+    println(company)
+
+    val firsWord = "Dicoding "
+    val lastWord = "Academy"
+    println(firsWord + lastWord)
+
+    val valueA: Int = 10
+    val valueB = 20
+    print(valueA + valueB)
+}
