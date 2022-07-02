a2f223b91bc7b88b1252cac46fec0dd21da79816
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-02 17:16:20 +0700

    Different println() & print()

--- Programming-With-IntelliJ-IDEA/Programming-With-IntelliJ-IDEA/src/main/kotlin/App.kt
+++ Programming-With-IntelliJ-IDEA/Programming-With-IntelliJ-IDEA/src/main/kotlin/App.kt
@@ -1,3 +1,15 @@
 fun main() {
     println("Hello Kotlin")
+
+    // single line comment
+    /*
+    multi line comment
+    Hello Kotlin
+     */
+
+    val name = "John"
+
+    print("Hello my name is ")
+    println(name)
+    print(if (true) "Always true" else "Always false")
 }
