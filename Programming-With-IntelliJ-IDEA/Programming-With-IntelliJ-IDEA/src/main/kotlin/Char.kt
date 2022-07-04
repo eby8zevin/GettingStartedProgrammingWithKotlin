45eb34095080d17a13a0e4e7de24c69506ab41f1
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-04 09:12:43 +0700

    Char

---
+++ Programming-With-IntelliJ-IDEA/Programming-With-IntelliJ-IDEA/src/main/kotlin/Char.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    /*val character = 'A'
+    val character: Char = 'ABC' // Incorrect character literal*/
+
+    var vocal = 'A' // Unicode Character “A” (U+0041)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+}
