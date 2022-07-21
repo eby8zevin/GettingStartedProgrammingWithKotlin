c4188cab18322dd18d303baf4a3fd091e4c8d3b7
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-21 09:21:08 +0700

    Kotlin Standard Library

---
+++ Programming-With-IntelliJ-IDEA/Programming-With-IntelliJ-IDEA/src/main/kotlin/kotlinFunctionalProgramming/KotlinStandardLibrary.kt
@@ -0,0 +1,26 @@
+package kotlinFunctionalProgramming
+
+fun main() {
+
+    println(buildString)
+
+    // lambda argument (it) = let, also
+    val text = "Hello"
+    text.let {
+        val message = "$it Kotlin"
+        println(message)
+    }
+
+    // can change (it)
+    val textChange = "Hello"
+    textChange.let { value ->
+        val message = "$value Kotlin"
+        println(message)
+    }
+}
+
+// lambda receiver (this) = run, with, apply
+val buildString = StringBuilder().apply {
+    append("Hello ")
+    append("Kotlin ")
+}
