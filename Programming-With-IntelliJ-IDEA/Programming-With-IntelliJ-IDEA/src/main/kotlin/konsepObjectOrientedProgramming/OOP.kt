dc125a599b7f5a43dd6d5350a7b84ef834284170
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-23 13:58:53 +0700

    Object-Oriented Programming

---
+++ Programming-With-IntelliJ-IDEA/Programming-With-IntelliJ-IDEA/src/main/kotlin/konsepObjectOrientedProgramming/OOP.kt
@@ -0,0 +1,12 @@
+package konsepObjectOrientedProgramming
+
+fun main() {
+
+    println("Blueprint: Mobil") // class
+
+    println("Komponen: Roda, Pintu, Nomor Kendaraan, & Lampu") // atribut
+
+    println("Kemampuan: Melaju & Mundur") // behaviour
+
+    println("The result of the realization of a blueprint is called an object")
+}
