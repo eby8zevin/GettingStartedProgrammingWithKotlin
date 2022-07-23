425705839390ad157fe47d800b821f8b70c2b911
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-23 14:41:04 +0700

    Membuat Kelas

---
+++ Programming-With-IntelliJ-IDEA/Programming-With-IntelliJ-IDEA/src/main/kotlin/konsepObjectOrientedProgramming/CreateClass.kt
@@ -0,0 +1,24 @@
+package konsepObjectOrientedProgramming
+
+class Animal(
+    val name: String,
+    val weight: Double,
+    val age: Int,
+    val isMammal: Boolean
+) {
+    fun eat() {
+        println("$name makan !")
+    }
+
+    fun sleep() {
+        println("$name tidur !")
+    }
+}
+
+fun main() {
+    // val nameOfObject = NameOfClass([property1], [property2])
+    val dicodingCat = Animal("Dicoding Miaw", 4.2, 2, true)
+    println("Nama: ${dicodingCat.name}, Berat: ${dicodingCat.weight}, Umur: ${dicodingCat.age}, mamalia: ${dicodingCat.isMammal}")
+    dicodingCat.eat()
+    dicodingCat.sleep()
+}
