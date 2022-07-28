48f8c34a26daa4602959fba41fdcbc29549b9e70
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-28 22:44:48 +0700

    Process, Thread, I/O-Bound

---
+++ Programming-With-IntelliJ-IDEA/Programming-With-IntelliJ-IDEA/src/main/kotlin/berkenalandenganCoroutines/ProcessThreadIOBound.kt
@@ -0,0 +1,25 @@
+package berkenalandenganCoroutines
+
+fun main() {
+
+    println(
+        "Process = sebuah aplikasi adalah serangkaian proses yang saling bekerja sama. \n" +
+                "Inter Process Communication (IPC), example pipes & soket. \n" +
+                "multitasking \n" +
+                "multiprocessing \n" +
+                "multiprocessing is a challenge for developers in developing an application"
+    )
+    println()
+    println(
+        "main different proses & thread = bahwa thread biasanya merupakan komponen dari suatu proses. \n" +
+                "Thread share resources such as memory and data. \n" +
+                "thread-local storage \n" +
+                "UI thread."
+    )
+    println()
+    println(
+        "I/O -> Bottlenecks = limited memory, processor speed, etc. \n" +
+                "I/O-bound is an algorithm that depends on the input or output devices. \n" +
+                "terapkanlah concurrency jika aplikasi yang Anda kembangkan punya ketergantungan dengan perangkat I/O."
+    )
+}
