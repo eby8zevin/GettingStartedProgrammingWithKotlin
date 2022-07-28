4dcc0b11edd741b058ae753a64cd126d154f087c
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-28 19:00:18 +0700

    Constraint Type Parameter

--- Programming-With-IntelliJ-IDEA/Programming-With-IntelliJ-IDEA/src/main/kotlin/kotlinGenerics/ConstraintTypeParameter.kt
+++ Programming-With-IntelliJ-IDEA/Programming-With-IntelliJ-IDEA/src/main/kotlin/kotlinGenerics/ConstraintTypeParameter.kt
@@ -2,23 +2,24 @@ package kotlinGenerics
 
 fun main() {
 
-    val numbers = ListNumber<Long>()
-    val numbers2 = ListNumber<Int>()
-    val numbers3 = ListNumber<String>() // error : Type argument is not within its bounds
+    //val numbers = ListNumber<Long>()
+    //val numbers2 = ListNumber<Int>()
+    //val numbers3 = ListNumber<String>() // error : Type argument is not within its bounds
 
     val numbers4 = listOf(1, 2, 3, 4, 5)
-    numbers4.sumNumber()
+    //numbers4.sumNumber()
     val names = listOf("dicoding", "academy")
-    names.sumNumber() // error : inferred type String is not a subtype of Number
+    //names.sumNumber() // error : inferred type String is not a subtype of Number
 }
 
-class ListNumber<T : Number> : List<T> {
+/*class ListNumber<T : Number> : List<T> {
     override fun get(index: Int): T {
-        /* .. */
+        *//* .. *//*
     }
-}
+}*/
 
 // Another example
+/*
 fun <T : Number> List<T>.sumNumber(): T {
-    /* .. */
-}
+    *//* .. *//*
+}*/
