e260b180e79d0d2dfbf83125d132bd50d0d90740
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-28 15:44:41 +0700

    Kotlin OOP / Membuat Package Baru

---
+++ Programming-With-IntelliJ-IDEA/Programming-With-IntelliJ-IDEA/src/main/kotlin/com/dicoding/oop/Main.kt
@@ -0,0 +1,24 @@
+package com.dicoding.oop
+
+import com.dicoding.oop.utils.PI
+import com.dicoding.oop.utils.factorial
+import com.dicoding.oop.utils.pow
+import com.dicoding.oop.utils.sayHello
+
+import com.dicoding.oop.utils.*
+
+fun main() {
+
+    //com.dicoding.oop.utils.sayHello()
+    sayHello()
+    println(factorial(4.0))
+    println(pow(3.0, 2.0))
+    println(PI)
+
+    println()
+
+    println(factorial(4.0))
+    println(pow(3.0, 2.0))
+    println(PI)
+    println(areaOfCircle(13.0)) // using all functions and variables
+}
