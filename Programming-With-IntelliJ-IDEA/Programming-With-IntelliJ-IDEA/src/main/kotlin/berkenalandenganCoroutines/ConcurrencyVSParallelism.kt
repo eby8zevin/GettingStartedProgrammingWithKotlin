f6d5167623afbffc6cdd3b9f01880b223ef4c2e4
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-28 22:17:56 +0700

    Concurrency vs Parallelism

---
+++ Programming-With-IntelliJ-IDEA/Programming-With-IntelliJ-IDEA/src/main/kotlin/berkenalandenganCoroutines/ConcurrencyVSParallelism.kt
@@ -0,0 +1,15 @@
+package berkenalandenganCoroutines
+
+fun main(){
+
+    println("concurrency is not parallelism")
+
+    println("Example: concurrency")
+    println("Barista = Cappuccino, 4 Latte, Espresso, 2 Green Tea, Chocolate, 3 White Coffee")
+    println()
+    println("Example: parallelism")
+    println("Barista 1 = Cappuccino, 4 Latte, Espresso")
+    println("Barista 2 = 2 Green Tea, Chocolate, 3 White Coffee")
+    println()
+    println("That is, parallelism can lead to concurrency, but concurrency can occur without parallelism.")
+}
