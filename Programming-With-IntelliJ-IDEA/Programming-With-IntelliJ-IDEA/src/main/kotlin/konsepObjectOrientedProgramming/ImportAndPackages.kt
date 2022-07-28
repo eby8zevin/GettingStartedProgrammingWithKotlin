2e633fbf9faeea1d444608d5d00bdda80a0784fe
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-28 15:27:06 +0700

    Import & Packages

---
+++ Programming-With-IntelliJ-IDEA/Programming-With-IntelliJ-IDEA/src/main/kotlin/konsepObjectOrientedProgramming/ImportAndPackages.kt
@@ -0,0 +1,27 @@
+package konsepObjectOrientedProgramming
+/*import packagename.ClassName
+import packagename.functionName
+import packagename.propertyName*/
+import kotlin.random.Random
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sqrt
+import kotlin.math.cos as cosinus
+import kotlin.math.sqrt as akar
+import kotlin.math.*
+
+fun main() {
+
+    val someInt = kotlin.random.Random(0).nextInt(1, 10)
+    println(someInt)
+
+    val someInt1 = Random(0).nextInt(1, 10)
+    println(someInt1)
+
+    println(PI)
+    println(cos(120.0))
+    println(sqrt(9.0))
+
+    println(cosinus(120.0))
+    println(akar(9.0))
+}
