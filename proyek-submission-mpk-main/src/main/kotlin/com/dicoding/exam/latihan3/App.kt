65af998d3587a46cf4b87b50831aab83f2579257
Ahmad Abu Hasan <ahmadabuhasan@mhs.stmik-yadika.ac.id>
2022-07-31 21:21:30 +0700

    Exercise 3 - Generics

--- proyek-submission-mpk-main/src/main/kotlin/com/dicoding/exam/latihan3/App.kt
+++ proyek-submission-mpk-main/src/main/kotlin/com/dicoding/exam/latihan3/App.kt
@@ -30,7 +30,15 @@ package com.dicoding.exam.latihan3
  *
  */
 fun <T> checkType(args: T): String {
-    return ""
+    return when (args) {
+        is Int -> return "Yes! it's Integer"
+        is String -> return "Yes! it's String"
+        is Boolean -> return "Yes! it's Boolean"
+        is Double -> return "Yes! it's Double"
+        is List<*> -> return "Yes! it's List"
+        is Map<*,*> -> return "Yes! it's Map"
+        else -> ""
+    }
 }
 
 fun main() {
